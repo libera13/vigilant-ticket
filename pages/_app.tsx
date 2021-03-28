07e3c99ac0d4afba344b260610d14199f73c79f2
Emil <emillibera@gmail.com>
2021-03-28 14:36:42 +0200

    feat: landing page and ticket component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import {ChakraProvider} from "@chakra-ui/react";
+import {AppProps} from "next/app";
+import {extendTheme} from "@chakra-ui/react"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac",
+    },
+}
+const theme = extendTheme({colors})
+
+function MyApp({Component, pageProps}) {
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
-import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+import { NextPage } from "next";
+import NextLink from "next/link";
+import { Button, Flex, Text, Link } from "@chakra-ui/react";
+import {Simulate} from "react-dom/test-utils";
 
-export default function Home() {
-  return (
-    <div>
-      HEllo
-    </div>
-  )
-}
+const IndexPage: NextPage = () => {
+    return (
+        <Flex direction="column" alignItems="center" padding="12rem">
+            <Flex direction="column" alignItems="center" marginBottom="6rem">
+                <Text fontSize="100px" fontWeight="800">
+                    The best conference ever.
+                </Text>
+                <Text fontSize="3xl" fontWeight="600">
+                    Totally free too.
+                </Text>
+            </Flex>
+            <NextLink href="/api/login" passHref>
+                <Link>
+                    <Button colorScheme="teal">
+                        Grab your ticket
+                    </Button>
+                </Link>
+            </NextLink>
+        </Flex>
+    );
+};
+
+export default IndexPage;
